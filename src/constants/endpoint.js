5e58e17046e3263064c6a2db5b3f24c7bf08c164
ImNewmann <newman1602@gmail.com>
2020-04-22 13:46:12 +0100

    Updating endpoint url

--- src/constants/endpoint.js
+++ src/constants/endpoint.js
@@ -1,3 +1,2 @@
 // export const endPoint = 'http://localhost:8888/wp-json/wp/v2' // Development
-// export const endPoint = 'http://admin.tomnewman.eu/wp-json/wp/v2' // Production
-export const endPoint = 'http://admin.bnewman.co.uk/wp-json/wp/v2'
+export const endPoint = 'http://admin.tomnewman.eu/wp-json/wp/v2' // Production
