ffd7986e22a4f97aae4c6be4c2259a4b51691d5d
ImNewmann <newman1602@gmail.com>
2021-11-03 14:43:26 +0000

    Adds meta to vue config

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,27 @@
 module.exports = {
-  css: {
-    loaderOptions: {
-      scss: {
-        prependData: `@import "@/assets/scss/variables/variables.scss";`
-      }
-    }
-  },
-  chainWebpack: (config) => {
-    const svgRule = config.module.rule('svg');
- 
-    svgRule.uses.clear();
- 
-    svgRule
-      .use('babel-loader')
-      .loader('babel-loader')
-      .end()
-      .use('vue-svg-loader')
-      .loader('vue-svg-loader');
-  },
+    css: {
+        loaderOptions: {
+            scss: {
+                prependData: `@import "@/assets/scss/variables/variables.scss";`,
+            },
+        },
+    },
+    chainWebpack: (config) => {
+        const svgRule = config.module.rule('svg');
+
+        svgRule.uses.clear();
+
+        svgRule
+            .use('babel-loader')
+            .loader('babel-loader')
+            .end()
+            .use('vue-svg-loader')
+            .loader('vue-svg-loader');
+
+        config.plugin('html').tap((args) => {
+            args[0].meta = { description: 'Director / London' };
+
+            return args;
+        });
+    },
 };
