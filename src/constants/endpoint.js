879da2be12551e1c292f6f0fd98d0741d1c930ec
Ben Newman <newman1602@gmail.com>
2022-09-30 15:47:36 +0100

    update endpoint to new domain

--- src/constants/endpoint.js
+++ src/constants/endpoint.js
@@ -1,2 +1,2 @@
 // export const endPoint = 'http://localhost:8888/wp-json/wp/v2' // Development
-export const endPoint = 'https://tomnewman.eu/admin/index.php/wp-json/wp/v2'; // Production
+export const endPoint = 'https://tomnewman.uk/admin/index.php/wp-json/wp/v2'; // Production
